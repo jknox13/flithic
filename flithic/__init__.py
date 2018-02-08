778517df9e5ebb80c0ac33247f33e74d66637404
Joseph Knox <jknox13@uw.edu>
2018-02-08 11:16:17 -0800

    add init skeletons and basic docs

---
+++ flithic/__init__.py
@@ -0,0 +1,23 @@
+"""
+FlItHiC: a FLexible ITerative HIerarchical Clustering package
+=============================================================
+
+Documentation is available in the docstrings.
+
+Contents
+--------
+sjkf
+
+Subpackages
+-----------
+Source code from `scipy.spatial.distance` was originally copied, and then
+modified so that the distance algorithms accept `nan`s. The relevant Scipy
+license (BSD3) has been included (see LICENSE.txt in ``flithic.distance``).
+
+Using this subpackage requires an explicit import. ie ``import flithic.distance``.
+
+::
+    distance            --- Distance algorithms altered from ``scipy.spatial``
+
+"""
+from __future__ import division, print_function, absolute_import
---
+++ flithic/distance/__init__.py
@@ -0,0 +1,20 @@
+"""
+=============================================
+Distance algorithms (:mod:`flithic.distance`)
+=============================================
+
+.. currentmodule:: flithic.distance
+
+Distance Funcitons
+==================
+These distance functions were initially taken verbatim from (:mod:`scipy.spatial`),
+and have since been altered to accept `nan` values.
+
+For documentation, see (:mod:`scipy.spatial`).
+
+For license, see LICENSE.txt
+"""
+
+from __future__ import division, print_function, absolute_import
+
+from . import distance
